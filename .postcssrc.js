46f97b73b9aadf1613634a9706844e2dcb05de61
Bae Ji-min <dncl46@gmail.com>
2023-09-08 21:58:43 +0900

    postcss config add

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,7 +1,23 @@
 const autoprefixer = require("autoprefixer")
+const postcssCombineMediaQuery = require("postcss-combine-media-query");
+const postcssCsso = require("postcss-csso")
 
 module.exports = {
   plugins: [
-    autoprefixer
+    autoprefixer,
+    postcssCombineMediaQuery,
+    postcssCsso({
+      restructure: false
+    })
   ]
 }
+
+// module.exports = {
+//   plugins: [
+//     require('autoprefixer'),
+//     require('postcss-combine-media-query'),
+//     require('postcss-csso')({
+//       restructure: false
+//     })
+//   ]
+// }
