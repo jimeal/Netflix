1d3ed8a13f29b7f34f6d03afed138487f69ed860
Bae Ji-min <dncl46@gmail.com>
2023-09-08 21:34:50 +0900

    upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import "../scss/main.scss";
+import formBlock from "./common.js";
+import loginForm from "./login.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  formBlock();
+  loginForm();
+})
