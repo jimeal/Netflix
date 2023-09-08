8eb62c48b78755d4d1802475d359815a1f26e0b9
Bae Ji-min <dncl46@gmail.com>
2023-09-08 21:37:18 +0900

    Revert "upload"
    This reverts commit 1d3ed8a13f29b7f34f6d03afed138487f69ed860.

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import "../scss/main.scss";
-import formBlock from "./common.js";
-import loginForm from "./login.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-  formBlock();
-  loginForm();
-})
